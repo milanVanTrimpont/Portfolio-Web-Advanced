bcb40a4f237b1f486b9389c809f3db90724aca90
milanVanTrimpont <163291204+milanVanTrimpont@users.noreply.github.com>
2024-06-05 14:09:20 +0200

    bmi calculator
    de calculatie om je bmi te berekenen en een constante aangemaakt om te kijken in welke categorie je valt

--- homepage.js
+++ homepage.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// constante maken voor de verschillende categorieen
+const bmi_categorie = {
+    ondergewicht: 18.5,
+    gezond: 25,
+    overgewicht: 30,
+    obesitas: 35,
+    ernstig_obees: 40
+};
 
 function validateForm() {
     let form = document.forms["bmi_berekenen"];
@@ -7,8 +15,9 @@ function validateForm() {
     let lengte = form["lengte"].value;
     let gewicht = form["gewicht"].value;
     var getal = /^[0-9]+$/;
-
+    let resultaat = document.getElementById("resultaat");
     
+    // kijken of alle waardes kloppen
     if (leeftijd === "" || lengte === "" || gewicht === "") 
     {
         alert("Je moet alle gegevens invullen");
@@ -27,5 +36,37 @@ function validateForm() {
         return false;
     }
 
-    return true;
+
+    // BMI Berekening
+    lengte = lengte / 100; 
+    let bmi = gewicht / (lengte * lengte);
+    // resultaat afkorten tot 1 cijfer achter de komma en weergeven
+    
+
+ // Categorie bepalen
+    let categorie = '';
+    switch (true) {
+        case (bmi < bmi_categorie.ondergewicht):
+            categorie = 'Ondergewicht';
+            break;
+        case (bmi <= bmi_categorie.gezond):
+            categorie = 'Gezond gewicht';
+            break;
+        case (bmi <= bmi_categorie.overgewicht):
+            categorie = 'Overgewicht';
+            break;
+        case (bmi <= bmi_categorie.obesitas):
+            categorie = 'Obesitas';
+            break;
+        case (bmi > bmi_categorie.ernstig_obees):
+            categorie = 'Ernstige obesitas';
+            break;
+        default:
+            categorie = 'Onbekend';
+            break;
+    }
+    resultaat.innerHTML = "Je hebt een BMI van: " + bmi.toFixed(1) + " en je valt in de categorie: " + categorie;
+
+    // zorgen dat bmi getoont wordt
+    return false;
 }
