a0eabaa658777c5ed25e67cd53780dd8bef2453e
milanVanTrimpont <163291204+milanVanTrimpont@users.noreply.github.com>
2024-06-07 16:46:24 +0200

    standaard waarden toegevoegd met localstorage

--- homepage.js
+++ homepage.js
@@ -114,7 +114,76 @@ async function bmiBerekening(lengte, gewicht) {
 
     return { bmi, categorie };
 }
-    
-    
 
+
+
+// standaard waarden man
+localStorage.setItem('leeftijdM', 18);
+localStorage.setItem('lengteM', 175);
+localStorage.setItem('gewichtM', 70);
+localStorage.setItem('bmiM', 22.9);
+localStorage.setItem('categorieM', 'gezond gewicht');
+//standaard waarden vrouw
+localStorage.setItem('leeftijdV', 18);
+localStorage.setItem('lengteV', 165);
+localStorage.setItem('gewichtV', 60);
+localStorage.setItem('bmiV', 22.0);
+localStorage.setItem('categorieV', 'gezond gewicht');
+//obesitas
+localStorage.setItem('leeftijdO', 18);
+localStorage.setItem('lengteO', 175);
+localStorage.setItem('gewichtO', 100);
+localStorage.setItem('bmiO', 32.7);
+localStorage.setItem('categorieO', 'obesitas');
+
+
+// ophalen van de waarden man
+localStorage.getItem('leeftijdM');
+localStorage.getItem('lengteM');
+localStorage.getItem('gewichtM');
+localStorage.getItem('bmiM');
+localStorage.getItem('categorieM');
+
+// ophalen van de waarden vrouw
+localStorage.getItem('leeftijdV');
+localStorage.getItem('lengteV');
+localStorage.getItem('gewichtV');
+localStorage.getItem('bmiV');
+localStorage.getItem('categorieV');
+
+// ophalen van de waarden obesitas
+localStorage.getItem('leeftijdO');
+localStorage.getItem('lengteO');
+localStorage.getItem('gewichtO');
+localStorage.getItem('bmiO');
+localStorage.getItem('categorieO');
+    
+    
+// weergeven als er op de knop gedrukt wordt
+document.getElementById('standaardWaardenM').addEventListener('click', () => 
+{   
+    document.getElementById("leeftijd").innerHTML = "Leeftijd: " + localStorage.getItem('leeftijdM') + " jaar";
+    document.getElementById("lengte").innerHTML = "Lengte: " + localStorage.getItem('lengteM') + " cm";
+    document.getElementById("gewicht").innerHTML = "Gewicht: " + localStorage.getItem('gewichtM') + " kg";
+    document.getElementById("bmi").innerHTML = "BMI: " + localStorage.getItem('bmiM');
+    document.getElementById("categorie").innerHTML = "Categorie: " + localStorage.getItem('categorieM');
+});
+
+document.getElementById('standaardWaardenV').addEventListener('click', () => 
+{   
+    document.getElementById("leeftijd").innerHTML = "Leeftijd: " + localStorage.getItem('leeftijdV') + " jaar";
+    document.getElementById("lengte").innerHTML = "Lengte: " + localStorage.getItem('lengteV') + " cm";
+    document.getElementById("gewicht").innerHTML = "Gewicht: " + localStorage.getItem('gewichtV') + " kg";
+    document.getElementById("bmi").innerHTML = "BMI: " + localStorage.getItem('bmiV');
+    document.getElementById("categorie").innerHTML = "Categorie: " + localStorage.getItem('categorieV');
+});
+
+document.getElementById('obesitas').addEventListener('click', () => 
+    {   
+        document.getElementById("leeftijd").innerHTML = "Leeftijd: " + localStorage.getItem('leeftijdO') + " jaar";
+        document.getElementById("lengte").innerHTML = "Lengte: " + localStorage.getItem('lengteO') + " cm";
+        document.getElementById("gewicht").innerHTML = "Gewicht: " + localStorage.getItem('gewichtO') + " kg";
+        document.getElementById("bmi").innerHTML = "BMI: " + localStorage.getItem('bmiO');
+        document.getElementById("categorie").innerHTML = "Categorie: " + localStorage.getItem('categorieO');
+    });
 
